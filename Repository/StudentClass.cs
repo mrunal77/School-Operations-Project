de6cce362ccada1f43500bc6953f1e9c377d9dc2
mrunal77 <mtmeshram77@outlook.com>
2023-09-10 11:46:24 +0530

    Student Class Repository

---
+++ Repository/StudentClass.cs
@@ -0,0 +1,14 @@
+﻿using School_Api_Project.DbContextDirectory;
+using School_Api_Project.Models;
+using School_Api_Project.Repository.Interfaces;
+
+namespace School_Api_Project.Repository
+{
+    public class StudentClassRepository : GenericRepository<StudentClass>, IStudentClassRepository
+    {
+        public StudentClassRepository(MTSchool_DbContext context) : base(context)
+        {
+
+        }
+    }
+}
