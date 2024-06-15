5911c367f3e65aa744ccd8f912b25d012be62b0a
mrunal77 <mtmeshram77@outlook.com>
2024-06-15 21:47:41 +0530

    SQL Scripts to add Student Class data to tables.

---
+++ SQLScripts/Insert_Table_Values.sql
@@ -0,0 +1,17 @@
+select * from INFORMATION_SCHEMA.TABLES
+
+select * from dbo.students
+
+
+select * from dbo.studentClasses
+
+insert into dbo.studentClasses values('1st')
+insert into dbo.studentClasses values('2nd')
+insert into dbo.studentClasses values('3rd')
+insert into dbo.studentClasses values('4th')
+insert into dbo.studentClasses values('5th')
+insert into dbo.studentClasses values('6th')
+insert into dbo.studentClasses values('7th')
+insert into dbo.studentClasses values('8th')
+insert into dbo.studentClasses values('9th')
+insert into dbo.studentClasses values('10th')
